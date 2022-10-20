fd3dcd34ad11471a89103330abdc0cd3b7008b70
Zaira Fuentes <zairafuentes@berkeley.edu>
2022-10-19 17:04:50 -0700

    functional post

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -2,20 +2,32 @@ import { useState } from 'react';
 import React from 'react';
 import Menu from './Menu';
 import Block from './Block';
+import { render } from '@testing-library/react';
 
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    // const [exper, setExper] = React.useState(null);
+    const [posts, setPosts] = React.useState([]); // TODO: edit this variable
+    const [p, setP] = React.useState(null)
+    // let posts = [];
+    function handleClick(color){
+        posts.push(color)
+        setPosts(posts)
+        setP(posts.slice(0).reverse().map((color) => { return (
+            <Block color = {color}></Block>
+        );
+    }))
+        console.log(posts)
+    }
+
 
     return (
         <div>
-            <Menu></Menu>
-
-            {/* Below is where all of your Blocks should render! */}
-            {posts}
+            <Menu handleClick = {handleClick}></Menu>
+            {p}
         </div>
     );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Color from './Color';
+// import Feed from './Feed';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
-
+const Menu = (props) => {
+  // const [posts, setPosts] = React.useState([]); // TODO: edit this variable
+  // let posts = [];
     return (
       <div className="colorOptions">
           {/* TODO */}
+          <Color color="red" handleClick = {props.handleClick}></Color>
+          <Color color="pink" handleClick = {props.handleClick}></Color>
+          <Color color="blue" handleClick = {props.handleClick}></Color>
+          <Color color="green" handleClick = {props.handleClick}></Color>
+
+
+
+
       </div>
     );
 }
